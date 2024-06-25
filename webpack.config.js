2df4024dcc51d8394364f1073a9a18bcff83ce8b
Serchinastico <github@serchinastico.com>
2024-06-25 21:18:10 +0200

    Update dependencies and clean some of the remanining references to game of life

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Game of Life",
+      title: "Threejs",
       filename: "index.html",
       template: "src/index.html",
     }),
